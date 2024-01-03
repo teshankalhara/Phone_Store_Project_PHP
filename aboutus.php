74d275e6c5221c70f30f00abcdcde5f3ab87acc6
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-01-04 00:27:21 +0530

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -10,7 +10,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <link rel="icon" type="image/x-icon" href="pic/logo.jpg">
-    
+
     <title>Apple.Mobiles</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -25,6 +25,9 @@ session_start();
     <!--header-->
 
     <!--content-->
+    <div class="container container-fluid p-5">
+        <h2>About Us Page</h2>
+    </div>
     <!--content-->
 
     <!--footer-->
