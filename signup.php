15bbeb615ea5a526f30b608557305c7221d7c61b
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-01-07 22:32:13 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -43,7 +43,6 @@
     <?php require_once('header.php'); ?>
 
     <p><br><br></p>
-    <p><br><br></p>
 
     <div class="container container-fluid mb-5">
         <div class="row">
@@ -61,29 +60,29 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <label for="f_name">First Name</label>
-                                    <input type="text" id="f_name" name="f_name" class="form-control">
+                                    <input type="text" id="f_name" name="f_name" class="form-control" required>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="f_name">Last Name</label>
-                                    <input type="text" id="l_name" name="l_name" class="form-control">
+                                    <input type="text" id="l_name" name="l_name" class="form-control" required>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="col-md-6">
                                     <label for="email">Email</label>
-                                    <input type="text" id="email" name="email" class="form-control">
+                                    <input type="text" id="email" name="email" class="form-control" required>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="password">Password</label>
-                                    <input type="password" id="password" name="password" class="form-control">
+                                    <input type="password" id="password" name="password" class="form-control" required>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="col-md-6">
                                     <label for="mobile">Mobile</label>
-                                    <input type="text" id="mobile" name="mobile" class="form-control">
+                                    <input type="text" id="mobile" name="mobile" class="form-control" maxlength="10" required>
                                 </div>
                                 <div class="col-md-6"></div>
                             </div>
@@ -91,20 +90,20 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <label for="address1">Address #1</label>
-                                    <input type="textarea" id="address1" name="address1" class="form-control">
+                                    <input type="textarea" id="address1" name="address1" class="form-control" required>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="col-md-12">
                                     <label for="address2">Address #2</label>
-                                    <input type="textarea" id="address2" name="address2" class="form-control">
+                                    <input type="textarea" id="address2" name="address2" class="form-control" required>
                                 </div>
                             </div>
 
                             <br><br>
                             <div class="text-center">
-                                <input type="button" class="w-100 btn btn-primary" value="Sign Up" name="signup" id="signup_btn" style="font-weight:500;">
+                                <input type="submit" class="w-100 btn btn-primary" value="Sign Up" name="signup" id="signup_btn" style="font-weight:500;">
                             </div>
                         </div>
                     </form>
