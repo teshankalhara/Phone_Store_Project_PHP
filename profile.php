e1ff93987ff8f5a6618f8083e98912ec027877e5
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-01-08 04:20:37 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['uid'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <link rel="icon" type="image/x-icon" href="pic/logo.jpg">
-    
+
     <title>Apple.Mobiles</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
