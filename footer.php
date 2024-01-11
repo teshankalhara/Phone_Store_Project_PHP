2383a79e6d52bfd24cd1cd3c7080b0340d0a585b
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-01-11 13:06:17 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<link href="img/favicon.ico" rel="icon">
+<link rel="icon" type="image/x-icon" href="pic/logo.jpg">
 
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
