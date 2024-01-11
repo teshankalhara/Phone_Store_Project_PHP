9a1294e9d15cc15ddd452c20324e86e4aca0634e
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-01-11 13:51:13 +0530

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
-<header class="p-3 bg-dark sticky-top">
+<header class="p-3 bg-dark sticky-top shadow-5-strong">
   <div class="container container-fluid">
     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start ml-4">
       <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none ml-4 mr-4">
@@ -10,10 +10,10 @@
       </a>
 
       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 mr-4 ml-4">
-        <li class="mr-4"><a href="index.php" class="nav-link px-2 text-white">Home</a></li>
-        <li class="mr-4"><a href="services.php" class="nav-link px-2 text-white">Service</a></li>
-        <li class="mr-4"><a href="aboutus.php" class="nav-link px-2 text-white">About</a></li>
-        <li class="mr-4"><a href="faqs.php" class="nav-link px-2 text-white">FAQs</a></li>
+        <li class="nav-item mr-4"><a href="index.php" <?php echo ($activePage == 'home') ? 'class="px-2 nav-link""' : 'class="nav-link px-2 text-white"'; ?>>Home</a></li>
+        <li class="nav-item mr-4"><a href="services.php" <?php echo ($activePage == 'services') ? 'class="px-2 nav-link""' : 'class="nav-link px-2 text-white"'; ?>>Service</a></li>
+        <li class="nav-item mr-4"><a href="aboutus.php" <?php echo ($activePage == 'about') ? 'class="px-2 nav-link""' : 'class="nav-link px-2 text-white"'; ?>>About</a></li>
+        <li class="nav-item mr-4"><a href="faqs.php" <?php echo ($activePage == 'faqs') ? 'class="px-2 nav-link""' : 'class="nav-link px-2 text-white"'; ?>>FAQs</a></li>
         <li <?php echo ($searchBar != 'true') ? 'class="d-none"' : 'class="ml-2 bg-dark text-white-50"'; ?> style="width:250px;"><input type="text" class="form-control bg-dark text-white" id="search" name="" placeholder="Type to search..." style="border-top-left-radius: 10px; border-bottom-left-radius: 10px; border-top-right-radius: 0px;border-bottom-right-radius: 0px; border:1px solid #3B71CA; font-size:medium;"></li>
         <li style="top:10px;" <?php echo ($searchBar != 'true') ? 'class="d-none"' : ''; ?>><button class="btn btn-primary" id="search_btn" name="search" style="border-top-left-radius: 0px; border-bottom-left-radius: 0px; "><i class="fa fa-search"></i></button></li>
       </ul>
@@ -53,7 +53,7 @@
         </div>
         <!--signup form-->
         ';
-        }
+      }
       if (isset($_SESSION['uid'])) {
         $namePro = $_SESSION['uname'];
         echo "<div id='shoppingcart' class='mr-4 col-4'>
@@ -81,7 +81,7 @@
                       </div>
                   </div>
                 </div>";
-        }
+      }
       ?>
       <!-- login or signup-->
     </div>
