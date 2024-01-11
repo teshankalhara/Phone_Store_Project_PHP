b1706b7624af16d217560008e59e799573cd4cb3
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-01-11 22:02:55 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,41 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <link rel="icon" type="image/x-icon" href="pic/logo.jpg">
-    
+
     <title>Apple.Mobiles</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/headerStyle.css" rel="stylesheet">
     <link href="css/body.css" rel="stylesheet">
-    <style>
-        .panel-primary>.panel-heading {
-            color: #fff;
-            background-color: #337ab7;
-            text-align: center;
-            font-size: larger;
-            padding: 10px;
-            font-weight: bold;
-            font-family: Arial, Helvetica, sans-serif;
-        }
 
-        .panel-body {
-            padding: 15px;
-        }
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
-        .panel {
-            margin-bottom: 20px;
-            background-color: #fff;
-            border: 2px solid black;
-            border-radius: 4px;
-            box-shadow: 0px 2px 8px #343A40;
-        }
-    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
-    <?php require_once('header.php'); ?>
+    <?php
+    $searchBar = '';
+    $activePage = '';
+    require_once('header.php'); ?>
 
     <p><br><br></p>
 
@@ -53,10 +37,10 @@
         <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-                <div class="panel panel-primary">
-                    <div class="panel-heading bg-dark">Signup Form</div>
+                <div class="card card-primary">
+                    <div class="card-heading bg-dark text-white text-center p-3" style="font-size:x-large;">Signup Form</div>
                     <form method="post" action="">
-                        <div class="panel-body">
+                        <div class="card-body">
                             <div class="row">
                                 <div class="col-md-6">
                                     <label for="f_name">First Name</label>
@@ -101,7 +85,7 @@
                                 </div>
                             </div>
 
-                            <br><br>
+                            <br>
                             <div class="text-center">
                                 <input type="button" class="w-100 btn btn-primary" value="Sign Up" name="signup" id="signup_btn" style="font-weight:500;">
                             </div>
